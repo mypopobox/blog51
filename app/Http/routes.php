71d983ca44cc7f2ca1f32b639c757f3757296705
yeung_chun_po <mypopobox@gmail.com>
2016-02-14 23:14:28 +0800

    add admin function in route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,26 @@
 |
 */
 
+// Blog pages
 get('/', function () {
     return redirect('/blog');
 });
 
 get('blog', 'BlogController@index');
 get('blog/{slug}', 'BlogController@showPost');
+
+// Admin area
+get('admin', function()	{
+	return redirect('/admin/post');
+});
+
+$router->group(['namespace' => 'Admin'. 'middleware' => 'auth'], function(){
+	resource('admin/post', 'PostController');
+	resource('admin/tag', 'TagController');
+	get('admin/upload', 'UploadController@index');
+});
+
+// logging in and out
+get('/auth/login', 'Auth/AuthController@getLogin');
+post('/auth/login', 'Auth/AuthController@postLogin');
+get('/auth/logout', 'Auth/AuthController@getLogout');

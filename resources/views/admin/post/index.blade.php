63bd448b855bf4e42dc28a0272e8e0dd07dc5e48
yeung_chun_po <mypopobox@gmail.com>
2016-02-17 22:59:25 +0800

    create post-index view

---
+++ resources/views/admin/post/index.blade.php
@@ -0,0 +1,19 @@
+@extendf('admin.layout')
+
+@section('content')
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-nd-offset-2">
+				<div class="panel panel-deault">
+					<div class="panel-healing">
+						<h3 class="panel-title">Post</h3>
+					</div>
+					<div class="panel-body">
+						TODO
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+@stop
